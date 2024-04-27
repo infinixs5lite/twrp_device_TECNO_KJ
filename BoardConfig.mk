82297f9d9aa75eb1651023456ea81640009cef88
N00BShaikh <107968141+IamAzmathullaShaikh@users.noreply.github.com>
2024-04-27 18:58:30 +0530

    Update with build broken snippet

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,6 +10,9 @@ DEVICE_PATH := device/tecno/KJ
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
 
+#NETWORK
+BUILD_BROKEN_USES_NETWORK := true
+
 # A/B
 AB_OTA_UPDATER := true
 AB_OTA_PARTITIONS += \
