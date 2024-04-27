2a4c4a1019c8c8689bf9c2b9c28839d126551c54
N00BShaikh <107968141+IamAzmathullaShaikh@users.noreply.github.com>
2024-04-27 12:28:50 +0530

    Replace OMNI with TWRP

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_TECNO-KJ6-user
-add_lunch_combo omni_TECNO-KJ6-userdebug
-add_lunch_combo omni_TECNO-KJ6-eng
+add_lunch_combo twrp_TECNO-KJ6-user
+add_lunch_combo twrp_TECNO-KJ6-userdebug
+add_lunch_combo twrp_TECNO-KJ6-eng
